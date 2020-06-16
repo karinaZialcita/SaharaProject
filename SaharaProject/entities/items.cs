d00012f47e8b5776a5ee3e86d5a12bbaab661321
karinaZialcita <59714163+karinaZialcita@users.noreply.github.com>
2020-06-16 15:38:25 -0700

    fixed list with key words

--- SaharaProject/entities/items.cs
+++ SaharaProject/entities/items.cs
@@ -10,31 +10,16 @@ namespace sahara.entities
 
         public double price { get; set; }
         public string itemName { get; set; }
-
+        public List<string> keyWords { get; set; }
         public string imagePath { get; set; }
         public Items() { }
-        public Items(string newItemName, double newPrice, string newImagePath)
+        public Items(string newItemName, double newPrice, string newImagePath, List<string> newKeyWords)
         {
             itemName = newItemName;
             price = newPrice;
             imagePath = newImagePath;
+            keyWords = newKeyWords;
         }
-        public List<Items> getList()
-        {
-            List<Items> listOfItems = new List<Items>()
-            {   new Items("BedrockRug", 457.39,"items/bedrockRug.jpg"),
-                new Items("Rose Rug", 356.99,"items/roseRug.jpg"),
-                new Items("Lacy Rug", 122.56,"items/lacyRug.jpg"),
-                new Items("Tropical Rug", 271.59,"items/tropicalRug.jpg"),
-                new Items("Fluffy Rug", 562.81,"items/fluffyRug.jpg"),
-                new Items("Brown Mat", 245.51,"items/brownMat.jpg"),
-                new Items("Tatami Mat", 423.45,"items/tatamiMat.jpg"),
-                new Items("Red Kilim Mat", 367.99,"items/redKilimMat.jpg"),
-                new Items("avocadomat", 454.36,"items/avocadoMat.jpg"),
-                new Items("Ivory Mat", 345.57,"items/ivoryMat.jpg")
 
-            };
-            return listOfItems;
-        }
     }
 }
--- SaharaProject/webpage_sahara.aspx.cs
+++ SaharaProject/webpage_sahara.aspx.cs
@@ -13,7 +13,7 @@ namespace sahara
     public partial class webpage_sahara : System.Web.UI.Page
     {
         Items item = new Items();
-        
+        List<Items> itemList = new List<Items>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -23,10 +23,40 @@ namespace sahara
             wrapper.Visible = false;
           
         }
+        public List<Items> GetItems()
+        {
+            List<Items> itemList = new List<Items>()
+            {
+                new Items("Bedrock Rug", 457.39, "items/bedrockRug.jpg", 
+                    new List<string>() { "rug","bed", "rock", "bedrock", "bedrock rug" }),
+                new Items("Rose Rug", 356.99, "items/roseRug.jpg",
+                    new List<string>() { "rug","rose", "rose rug" }),
+                new Items("Lacy Rug", 122.56, "items/lacyRug.jpg",
+                    new List<string>() { "rug","lacy", "bedrock rug" }),
+                new Items("Tropical Rug", 271.59, "items/tropicalRug.jpg",
+                    new List<string>() { "rug","tropical", "tropical rug" }),
+                new Items("Fluffy Rug", 562.81, "items/fluffyRug.jpg",
+                    new List<string>() { "rug","fluffy", "fluffy rug" }),
 
+
+                new Items("Brown Mat", 245.51, "items/brownMat.jpg",
+                    new List<string>() { "mat","brown", "brown mat" }),
+                new Items("Tatami Mat", 423.45, "items/tatamiMat.jpg",
+                    new List<string>() { "mat","tatami", "tatami mat" }),
+                new Items("Red Kilim Mat", 367.99, "items/redKilimMat.jpg",
+                    new List<string>() { "mat","red", "red kilim", "kilim", "red kilim mat", "kilim mat" }),
+                new Items("Avocado mat", 454.36, "items/avocadoMat.jpg",
+                    new List<string>() { "mat","avocado", "avocado mat" }),
+                new Items("Ivory Mat", 345.57, "items/ivoryMat.jpg",
+                    new List<string>() { "mat","ivory", "ivory mat" })
+
+
+            };
+            return itemList;
+        }
         protected void onClickSearch(object sender, EventArgs e)
         {
-            List<Items> itemList = item.getList();
+            
             foreach (Items anItem in itemList)
             {
                 if (searchItem.Text.ToLower().Trim() == anItem.itemName.ToLower() ||
